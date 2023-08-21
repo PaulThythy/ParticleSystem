9f32d8a875f167de81a6803c15391b793f712494
Paul <58293620+PaulThythy@users.noreply.github.com>
2023-08-21 17:28:32 +0200

    vector2 class added, I plan to change how velocity works

---
+++ include/vector2.h
@@ -0,0 +1,24 @@
+//
+// Created by paul on 21/08/23.
+//
+
+#ifndef PARTICLESYSTEM_VECTOR2_H
+#define PARTICLESYSTEM_VECTOR2_H
+
+class Vector2{
+private:
+    float x;
+    float y;
+
+public:
+    Vector2(const float _x, const float _y): x(_x), y(_y) {}
+    Vector2(): x(0.0f), y(0.0f){}
+
+    void setX(const float _x);
+    void setY(const float _y);
+    
+    float getX();
+    float getY();
+};
+
+#endif //PARTICLESYSTEM_PARTICLES_H
---
+++ src/vector2.cpp
@@ -0,0 +1,12 @@
+//
+// Created by paul on 21/08/23.
+//
+
+#include <iostream>
+#include "../include/vector2.h"
+
+void Vector2::setX(const float _x) { x = _x; }
+void Vector2::setY(const float _y) { y = _y; }
+
+float Vector2::getX() { return x; }
+float Vector2::getY() { return y; }
