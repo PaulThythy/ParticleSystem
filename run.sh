57be2751af7cd1538f579d2f79b1c3924b67fd36
PaulThythy <paul.thi@hotmail.fr>
2023-09-18 15:45:06 +0200

    small modifications in the CMakeLists, run.sh and .gitignore, build directory changed depending on the build mode

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #Create a build directory and navigate into it
-rm -rf cmake-build-debug
-mkdir cmake-build-debug
-cd cmake-build-debug
+rm -rf build
+mkdir build
+
+build_type=Debug 
 
 #Generate the Makefile using CMake
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
+cmake -S . -B build/$build_type -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$build_type
+cmake --build build/$build_type --target ParticleSystem
 
-#build the project using CMake
-make
 
 #Check if the compilation was successful
 if [ $? -eq 0 ]; then
 	echo "Compilation successful. Running the ParticleSystem..."
 	#Execute the ParticleSystem binary
-	./ParticleSystem
+	./build/*/ParticleSystem
 else
 	echo "Compilation failed. Please check the error messages."
 fi
